7badf119184a876f3733389be4d5bfc630e9aab9
OviOvocny <werint.pp@gmail.com>
2025-06-18 11:21:33 +0200

    remove unneeded routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,12 +12,8 @@
 const Home = r => require.ensure([], () => r(require('@/pages/Home.vue')))
 const Projekty = r => require.ensure([], () => r(require('@/pages/Projekty.vue')))
 const Preklad = r => require.ensure([], () => r(require('@/pages/Preklad.vue')))
-const Navrhy = r => require.ensure([], () => r(require('@/pages/Navrhy.vue')))
 const Podcast = r => require.ensure([], () => r(require('@/pages/Podcast.vue')))
 const Tym = r => require.ensure([], () => r(require('@/pages/Tym.vue')))
-const Prihlaska = r => require.ensure([], () => r(require('@/pages/Prihlaska.vue')))
-const Zpravy = r => require.ensure([], () => r(require('@/pages/Zpravy.vue')))
-const FAQ = r => require.ensure([], () => r(require('@/pages/FAQ.vue')))
 const About = r => require.ensure([], () => r(require('@/pages/About.vue')))
 const Survival = r => require.ensure([], () => r(require('@/pages/Survival.vue')))
 
@@ -70,52 +66,12 @@ const routes = [
     }
   },
   {
-    path: '/navrhy',
-    name: 'Návrhy',
-    component: Navrhy,
-    meta: {
-      title: 'Návrhy | Bio-senpai',
-      icon: 'star'
-      // navHidden: true
-    }
-  },
-  {
     path: '/tym/:member?',
     name: 'Tým',
     component: Tym,
     meta: {
       title: 'Tým | Bio-senpai',
       icon: 'account-multiple'
-    }
-  },
-  {
-    path: '/prihlaska',
-    name: 'Přidat se',
-    component: Prihlaska,
-    alias: '/join',
-    meta: {
-      title: 'Přidat se | Bio-senpai',
-      icon: 'account-plus'
-    }
-  },
-  {
-    path: '/zpravy',
-    name: 'Zprávy',
-    component: Zpravy,
-    alias: '/kontakt',
-    meta: {
-      title: 'Zprávy | Bio-senpai',
-      icon: 'message'
-    }
-  },
-  {
-    path: '/faq/:category?',
-    name: 'FAQ',
-    component: FAQ,
-    meta: {
-      title: 'FAQ | Bio-senpai',
-      icon: 'library',
-      categorized: true
     }
   },
   {
